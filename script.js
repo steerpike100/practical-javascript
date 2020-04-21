91ee508a77ff5443c171cdba3740e340fd7cfd75
steveb <stevejbartholomew@gmail.com>
2020-04-21 11:26:48 +0100

    Complete Version 7

--- script.js
+++ script.js
@@ -57,3 +57,16 @@ var todoList = {
     this.displayTodos();
   },
 };
+
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+displayTodosButton.addEventListener('click', function(){
+  todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function(){
+  todoList.toggleAll();
+})
+

6f7bea8bd8ab1377eb148e17548789faf06d0f77
steveb <stevejbartholomew@gmail.com>
2020-04-21 10:48:35 +0100

    Version 6

--- index.js
+++ index.js
@@ -34,4 +34,26 @@ var todoList = {
     todo.completed = !todo.completed;
     this.displayTodos();
   },
+  toggleAll: function () {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    //Get number of completed todos
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    //Case 1: if everything is true, make everything false.
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+      //Case 2: Otherwise make everything true
+    } else {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = true;
+      }
+    }
+    this.displayTodos();
+  },
 };
